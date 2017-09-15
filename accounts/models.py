7d1c2475265064b352c4838cc1f33463d6fb745a
Farbod909 <rafezyfarbod@gmail.com>
2017-09-15 13:42:34 -0700

    Complete addition of custom User model

--- accounts/models.py
+++ accounts/models.py
@@ -1,17 +1,20 @@
 from django.db import models
 from django.core.mail import send_mail
 from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.utils.translation import ugettext_lazy as _
+
 
 from .managers import UserManager
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_('email address'), unique=True)
     first_name = models.CharField(_('first name'), max_length=30, blank=False)
     last_name = models.CharField(_('last name'), max_length=30, blank=False)
     phone_number = models.CharField(_('phone number'), max_length=15, blank=False)
     date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
     is_active = models.BooleanField(_('active'), default=True)
+    is_staff = models.BooleanField(_('staff'), default=False)
 
     objects = UserManager()
 
--- parkingapp/settings.py
+++ parkingapp/settings.py
@@ -35,14 +35,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'parking.apps.ParkingConfig',
-    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
+    'parking.apps.ParkingConfig',
 ]
 
 MIDDLEWARE = [
