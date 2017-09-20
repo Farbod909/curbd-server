0236d3833153177dfab98886e8bfaac48f56584b
Farbod909 <rafezyfarbod@gmail.com>
2017-09-20 00:10:45 -0700

    Add RepeatingAvailabilityAdmin

--- parking/admin.py
+++ parking/admin.py
@@ -19,7 +19,15 @@ class FixedAvailabilityAdmin(admin.ModelAdmin):
     get_pricing.short_description = 'price'
 
 
+class RepeatingAvailabilityAdmin(admin.ModelAdmin):
+    list_display = ('parking_space', 'start_time', 'end_time', 'repeating_days', 'get_pricing')
+
+    def get_pricing(self, obj):
+        return '$' + str((obj.pricing * 12)/100) + ' / hr'
+    get_pricing.short_description = 'price'
+
+
 admin.site.register(ParkingSpace)
 admin.site.register(FixedAvailability, FixedAvailabilityAdmin)
-admin.site.register(RepeatingAvailability)
+admin.site.register(RepeatingAvailability, RepeatingAvailabilityAdmin)
 admin.site.register(Reservation)
