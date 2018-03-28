b31477df1eaec14eabfbb126d4c96c3bab468883
Farbod909 <rafezyfarbod@gmail.com>
2018-03-28 02:46:07 -0700

    Reorganize API viewset placement

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from . import views
+from accounts import views as account_views
+
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
+router.register(r'users', account_views.UserViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
 ]
