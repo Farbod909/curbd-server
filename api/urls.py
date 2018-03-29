ae1399d82b2035cc6dee012d81d7a549cca0538d
Farbod909 <rafezyfarbod@gmail.com>
2018-03-28 22:11:34 -0700

    Added root API view manually

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url, include
 from rest_framework import routers
+from . import views
 
 
 router = routers.DefaultRouter()
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    url(r'^$', views.api_root),
     url(r'^auth/', include('rest_framework.urls'), name='rest_framework'),
     url(r'^users/', include('accounts.api_urls')),
 ]
