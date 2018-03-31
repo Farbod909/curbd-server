01ac7cff89213cfa31c5e9de33eac3941f4e4ff5
Farbod909 <rafezyfarbod@gmail.com>
2018-03-31 15:57:41 -0700

    Remove unused import which was added for testing purposes

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,7 +2,6 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from .models import Customer, Host, Car
-from parking.models import Reservation
 
 
 class UserListSerializer(serializers.ModelSerializer):
