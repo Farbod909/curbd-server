178e984bfd84c09cae5fc2a27e38874e230d98b4
Farbod909 <rafezyfarbod@gmail.com>
2018-04-01 01:12:55 -0700

    Add filter backend for previous commit

---
+++ parking/api_filters.py
@@ -0,0 +1,45 @@
+import dateutil.parser
+from django.db.models.query import Q
+import pytz
+from rest_framework import filters
+
+
+class LocationAndTimeAvailableFilter(filters.BaseFilterBackend):
+    """
+    Filter the returned parking spaces to a given location and time
+    """
+
+    def filter_queryset(self, request, queryset, view):
+        bottom_left_lat = request.query_params.get('bl_lat', None)
+        bottom_left_long = request.query_params.get('bl_long', None)
+        top_right_lat = request.query_params.get('tr_lat', None)
+        top_right_long = request.query_params.get('tr_long', None)
+        start_datetime_iso = request.query_params.get('start', None)
+        end_datetime_iso = request.query_params.get('end', None)
+
+        location_params = [
+            bottom_left_lat, bottom_left_long,
+            top_right_lat, top_right_long,
+        ]
+        time_params = [start_datetime_iso, end_datetime_iso]
+
+        if not any([item is None for item in location_params]):
+            # limit queryset to parking spaces within a coordinate box
+            queryset = queryset.filter(
+                Q(longitude__gte=bottom_left_long),
+                Q(longitude__lte=top_right_long),
+                Q(latitude__gte=bottom_left_lat),
+                Q(latitude__lte=top_right_lat))
+
+        if not any([item is None for item in time_params]):
+            pst = pytz.timezone('US/Pacific')  # TODO: Determine time zone based on coordinates
+
+            start_datetime = pst.localize(dateutil.parser.parse(start_datetime_iso))
+            end_datetime = pst.localize(dateutil.parser.parse(end_datetime_iso))
+
+            # limit queryset to parking spaces with
+            # vacant spots in the specified datetime range.
+            queryset = [parkingspace for parkingspace in queryset
+                        if parkingspace.unreserved_spaces(start_datetime, end_datetime) > 0]
+
+        return queryset
