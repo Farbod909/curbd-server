b373abc766f19939f1a32c5827b08a0cf4d94b5e
Farbod909 <rafezyfarbod@gmail.com>
2018-04-02 14:04:15 -0700

    Add Token authentication

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import include
 from django.urls import path
+from rest_framework.authtoken import views as drf_views
+
 from . import views
 
 
 urlpatterns = [
     path('', views.api_root),
     path('auth/', include('rest_framework.urls'), name='rest_framework'),
+    path('auth/token', drf_views.obtain_auth_token, name='auth'),
     path('accounts/', include('accounts.api_urls')),
     path('parking/', include('parking.api_urls')),
 ]
