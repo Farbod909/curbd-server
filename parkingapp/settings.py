9999a6fb9666bd30620549ea72bf44bb8ebf13ee
Farbod909 <rafezyfarbod@gmail.com>
2018-04-02 23:03:31 -0700

    React finally works on home page

--- parkingapp/settings.py
+++ parkingapp/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'django_filters',
+    'webpack_loader',
 ]
 
 REST_FRAMEWORK = {
@@ -51,6 +52,13 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
+}
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+            'BUNDLE_DIR_NAME': 'bundles/',
+            'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.dev.json'),
+        }
 }
 
 MIDDLEWARE = [
--- parkingapp/urls.py
+++ parkingapp/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
-
-from . import views
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
