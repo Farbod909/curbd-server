8b3c8fcecb04817252c635094e3ce00d2ad9229e
Farbod909 <rafezyfarbod@gmail.com>
2019-03-18 01:09:46 -0700

    Add post password reset confirmation email

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,6 +1,6 @@
 from django.core.mail import send_mail
 from django.dispatch import receiver
-from django_rest_passwordreset.signals import reset_password_token_created
+from django_rest_passwordreset.signals import reset_password_token_created, post_password_reset
 
 
 @receiver(reset_password_token_created)
@@ -19,3 +19,17 @@ def handle_reset_password_token_created(sender, reset_password_token, *args, **k
         "If you did not request a password reset, please ignore this email.".format(reset_password_url),
         "no-reply@curbdparking.com",
         [reset_password_token.user.email])
+
+
+@receiver(post_password_reset)
+def handle_post_password_reset(sender, user, *args, **kwargs):
+    """
+    Handles informing user that password was reset
+    An e-mail needs to be sent to the user
+    """
+
+    send_mail(
+        "Curbd Password Reset Successful",
+        "Your password was reset successfully. If this was not you please contact support@curbdparking.com immediately.",
+        "no-reply@curbdparking.com",
+        [user.email])
